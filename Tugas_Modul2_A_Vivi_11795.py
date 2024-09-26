60552d103d2f6b0932edc65070590c1a779f091f
vivianpp <220711795@students.uajy.ac.id>
2024-09-26 15:44:24 +0700

    Update Tugas_Modul2_A_Vivi_11795.py

--- Tugas_Modul2_A_Vivi_11795.py
+++ Tugas_Modul2_A_Vivi_11795.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model='GBT_heartDisease_model.pkl'
+model=r'GBT_heartDisease_model.pkl'
 
 with open(model, 'rb') as f:
     loaded_model = pickle.load(f)
